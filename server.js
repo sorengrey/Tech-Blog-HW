f8e8f220ca4cc102e6280f0e91185a1c62502cc7
Heather Smith <halexsmith86@gmail.com>
2021-03-22 15:01:49 -0400

    Fixed dependencies and added gitignore

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 
 require('dotenv').config
-
+const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -11,10 +13,32 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
-
-
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => 
     console.log(`App listening on port ${PORT}!`));
 });
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
