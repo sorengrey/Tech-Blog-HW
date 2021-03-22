2267879866e3b2f3f1160ee9bd91419db6890f15
Heather Smith <halexsmith86@gmail.com>
2021-03-22 15:30:27 -0400

    Added utils directory

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// This was copied from the miniproject and has not been edited yet
+const withAuth = (req, res, next) => {
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
