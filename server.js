6254499fdcb36f2d1be2f41a50b7987b33d2f032
Heather Smith <halexsmith86@gmail.com>
2021-03-23 14:59:37 -0400

    Added directories: public, controllers/api

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
-
-require('dotenv').config
 const path = require('path');
+const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => 
-    console.log(`App listening on port ${PORT}!`));
-});
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
@@ -42,3 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
