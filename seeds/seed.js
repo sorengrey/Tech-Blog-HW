83aee814275059b26899352b5356beefa1d942e1
Heather Smith <halexsmith86@gmail.com>
2021-03-25 15:57:30 -0400

    Fixed some seed errors

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,25 +1,18 @@
 const sequelize = require('../config/connection');
 const { User, Blog } = require('../models');
 
-const userData = require('./userData.json');
-const blogData = require('./blogData.json');
+const userData = require('./userData');
+const blogData = require('./blogData');
 
-const seedDatabase = async () => {
+const seedAll = async () => {
   await sequelize.sync({ force: true });
-
-  const users = await User.bulkCreate(blogData, {
-    individualHooks: true,
-    returning: true,
-  });
-
-  for (const blog of blogData) {
-    await Blog.create({
-      ...blog,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
-
+  console.log('\n----- DATABASE SYNCED -----\n');
+  await userData;
+  console.log('\n----- User Data SEEDED -----\n');
+  await blogData;
+  console.log('\n----- Blog Data SEEDED -----\n');
+  
   process.exit(0);
-};
-
-seedDatabase();
+  };
+  
+  seedAll();
